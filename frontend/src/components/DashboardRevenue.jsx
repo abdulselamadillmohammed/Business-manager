c67d76b6d776f32966cd4c25ffb6e979d9db3c22
Ashadowmonarch <forcoding39@gmail.com>
2024-10-20 15:23:09 -0400

    finish editing revenue card

--- frontend/src/components/DashboardRevenue.jsx
+++ frontend/src/components/DashboardRevenue.jsx
@@ -1,19 +1,35 @@
 import styles from "../assets/css/dashboardRevenue.module.css";
 import logo from "../assets/icons/revenueCardIcon.png";
+import upArrow from "../assets/icons/revenueCardUpArrow.png";
 export default function DashboardRevenue() {
   return (
     <>
       <div className={styles.dashboardRevenueContainer}>
-        <div className={styles.iconContainer}>
-          <img
-            className={styles.revenueCardIcon}
-            src={logo}
-            alt="revenueCardIcon"
-          />
+        <div className={styles.revenueCardContainerTopSection}>
+          <div className={styles.iconContainer}>
+            <img
+              className={styles.revenueCardIcon}
+              src={logo}
+              alt="revenueCardIcon"
+            />
+          </div>
+
+          <div className={styles.revenueCardTextContainer}>
+            <p className={styles.revenueCardSupportingText}>REVENUE</p>
+            <p className={styles.revenueCardMainText}>$4,571.32</p>
+          </div>
+          <div className={styles.revenueCardPercentageContainer}>
+            <p className={styles.revenueCardPercentage}>19.2 %</p>
+            {/* Make state conditional based on percentage */}
+            <img
+              className={styles.revenueCardArrowUp}
+              src={upArrow}
+              alt="Arrow up"
+            />
+          </div>
         </div>
-        <div className={styles.revenueCardTextContainer}>
-          <p className={styles.revenueCardSupportingText}>REVENUE</p>
-          <p className={styles.revenueCardMainText}>$4,571.32</p>
+        <div className={styles.revenueCardContainerBottomSection}>
+          <p className={styles.revenueCardViewMore}>View more &gt;</p>
         </div>
       </div>
     </>
