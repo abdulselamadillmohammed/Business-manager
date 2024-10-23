be743ac80aa7ba477f9035093a559f8ca6160544
Ashadowmonarch <forcoding39@gmail.com>
2024-10-22 21:35:32 -0400

    Add dashboard navigator to sidebar

--- frontend/src/components/DashboardSidebar.jsx
+++ frontend/src/components/DashboardSidebar.jsx
@@ -3,7 +3,9 @@ import styles from "../assets/css/dashboardSidebar.module.css";
 export default function DashboardSidebar() {
   return (
     <>
-      <div className={styles.dashboardSidebarContainer}></div>
+      <div className={styles.dashboardSidebarContainer}>
+        <div className="dashboardNavigationContainer"></div>
+      </div>
     </>
   );
 }
