f771d0d4eccb81ab4b4a4a0b0c3f106b9b94b851
Ashadowmonarch <forcoding39@gmail.com>
2024-11-15 19:34:18 -0500

    Edit login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import React, { useState } from "react"; // Import useState
 import { Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
@@ -13,8 +14,11 @@ function App() {
   return (
     <Routes>
       <Route path="/signup" element={<SignupPage />} />
+
       <Route path="/login" element={<LoginPage setToken={setToken} />} />
+
       <Route path="/dashboard" element={<DashboardPage />} />
+
       <Route path="/transactions" element={<TransactionsPage />} />
       <Route path="/settings" element={<SettingsPage />} />
       <Route path="/logout" element={<LogoutPage />} />
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,11 @@
 // src/pages/LoginPage.jsx
 import React, { useState } from "react";
 import { useLogin } from "../services/mutations";
+import styles from "../assets/css/loginPage.module.css";
+import { useNavigate } from "react-router-dom";
 
-function LoginPage({ setToken }) {
+export default function LoginPage({ setToken }) {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({ username: "", password: "" });
   const { mutate, isSuccess, isError, error } = useLogin();
 
@@ -10,40 +13,75 @@ function LoginPage({ setToken }) {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
-
   const handleLogin = (e) => {
     e.preventDefault();
     mutate(formData, {
       onSuccess: (data) => {
         setToken(data.access);
         localStorage.setItem("refreshToken", data.refresh);
+        navigate("/dashboard");
+      },
+
+      // Show pop up error message and hold for 3 seconds
+      onError: () => {
+        navigate("/login");
       },
     });
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        onChange={handleInputChange}
-        value={formData.username}
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleInputChange}
-        value={formData.password}
-        required
-      />
-      <button type="submit">Login</button>
-      {isSuccess && <p>Login successful!</p>}
-      {isError && <p>{error.response?.data?.message || "Login failed"}</p>}
-    </form>
+    <div className={styles.loginPageContainer}>
+      <p className={styles.loginPageHeading}>Log In to your Account</p>
+      <form className={styles.loginForm} onSubmit={handleLogin}>
+        <div className={styles.loginInputContainer}>
+          <label className={styles.loginLabel} htmlFor="username">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className={styles.loginInput}
+            placeholder="Username"
+            onChange={handleInputChange}
+            value={formData.username}
+            required
+          />
+          <p className={styles.lowerInputLabel}>Please enter your username</p>
+        </div>
+        <div className={styles.loginInputContainer}>
+          <label className={styles.loginLabel} htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className={styles.loginInput}
+            placeholder="Password"
+            onChange={handleInputChange}
+            value={formData.password}
+            required
+          />
+          <p className={styles.lowerInputLabel}>Please enter your password</p>
+        </div>
+        <button className={styles.loginFormButton} type="submit">
+          Login
+        </button>
+
+        <p className={styles.forgotPasswordRedirect}>Forgot Password?</p>
+        <div className={styles.signupRedirectContainer}>
+          <p>Don’t have an account?</p>
+          <p
+            className={styles.signupRedirect}
+            onClick={() => {
+              navigate("/signup");
+            }}
+          >
+            Sign up
+          </p>
+        </div>
+      </form>
+    </div>
   );
 }
-
-export default LoginPage;
